49a9559830b459b0405bc868ce59cc459620453f
RoscaSilviu <silviu.rosca@student.unitbv.ro>
2025-01-04 14:07:59 +0200

    Changed lighting settings, added brakes , added more houses and section full of houses

--- Assets/Scripts/Car/Car/CarHandler.cs
+++ Assets/Scripts/Car/Car/CarHandler.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CarHandler : MonoBehaviour
@@ -8,67 +10,86 @@ public class CarHandler : MonoBehaviour
     [SerializeField]
     Transform gameModel;
 
+    [SerializeField]
+    MeshRenderer carMeshRender;
+
+    //Max values
     float maxSteerVelocity = 2;
     float maxForwardVelocity = 30;
 
+    //Multipliers
     float accelerationMultiplier = 3;
     float breaksMultiplier = 15;
     float steeringMultiplier = 5;
 
+    //Input
     Vector2 input = Vector2.zero;
 
+    //Emissive property
+    int _EmissionColor = Shader.PropertyToID("_EmissionColor");
+    Color emissiveColor = Color.white;
+    float emissiveColorMultiplier = 0f;
+
+    // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+
     }
 
+    // Update is called once per frame
     void Update()
     {
+        //Rotate car model when "turning"
         gameModel.transform.rotation = Quaternion.Euler(0, rb.linearVelocity.x * 5, 0);
+
+        if (carMeshRender != null)
+        {
+            float desiredCarEmissiveColorMultiplier = 0f;
+
+            if (input.y < 0)
+                desiredCarEmissiveColorMultiplier = 4.0f;
+
+            emissiveColorMultiplier = Mathf.Lerp(emissiveColorMultiplier, desiredCarEmissiveColorMultiplier, Time.deltaTime * 4);
+
+            carMeshRender.material.SetColor(_EmissionColor, emissiveColor * emissiveColorMultiplier);
+        }
     }
 
     private void FixedUpdate()
     {
+        //Apply Acceleration
         if (input.y > 0)
-        {
             Accelerate();
-        }
         else
-        {
             rb.linearDamping = 0.2f;
-        }
 
+        //Apply Brakes
         if (input.y < 0)
-        {
             Brake();
-        }
 
         Steer();
 
+        //Force the car not to go backwards
         if (rb.linearVelocity.z <= 0)
-        {
             rb.linearVelocity = Vector3.zero;
-        }
     }
 
     void Accelerate()
     {
         rb.linearDamping = 0;
 
+        //Stay within the speed limit
         if (rb.linearVelocity.z >= maxForwardVelocity)
-        {
             return;
-        }
 
         rb.AddForce(rb.transform.forward * accelerationMultiplier * input.y);
     }
 
     void Brake()
     {
+        //Don't brake unless we are going forward
         if (rb.linearVelocity.z <= 0)
-        {
             return;
-        }
 
         rb.AddForce(rb.transform.forward * breaksMultiplier * input.y);
     }
@@ -77,19 +98,24 @@ public class CarHandler : MonoBehaviour
     {
         if (Mathf.Abs(input.x) > 0)
         {
+            //Move the car sideways
             float speedBaseSteerLimit = rb.linearVelocity.z / 5.0f;
             speedBaseSteerLimit = Mathf.Clamp01(speedBaseSteerLimit);
 
             rb.AddForce(rb.transform.right * steeringMultiplier * input.x * speedBaseSteerLimit);
 
+            //Normalize the X Velocity
             float normalizedX = rb.linearVelocity.x / maxSteerVelocity;
 
+            //Ensure that we don't allow it to get bigger than 1 in magnitued. 
             normalizedX = Mathf.Clamp(normalizedX, -1.0f, 1.0f);
 
+            //Make sure we stay within the turn speed limit
             rb.linearVelocity = new Vector3(normalizedX * maxSteerVelocity, 0, rb.linearVelocity.z);
         }
-        else
+        else 
         {
+            //Auto center car
             rb.linearVelocity = Vector3.Lerp(rb.linearVelocity, new Vector3(0, 0, rb.linearVelocity.z), Time.fixedDeltaTime * 3);
         }
     }
