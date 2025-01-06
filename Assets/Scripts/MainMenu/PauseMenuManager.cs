08ce740884dfac2896da24886a40c812cd5e877c
RoscaSilviu <silviu.rosca@student.unitbv.ro>
2025-01-06 19:25:31 +0200

    Beggining of a pause menu (kinda)

---
+++ Assets/Scripts/MainMenu/PauseMenuManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PauseMenuManager : MonoBehaviour
+{
+    public static PauseMenuManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // Păstrează meniul între scene
+        }
+        else
+        {
+            Destroy(gameObject); // Distruge instanțele duplicate
+        }
+    }
+}
