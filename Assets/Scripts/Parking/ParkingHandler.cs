81d88749aa845e2b96813b018d9e0cdaf4147ce1
Stoica-David <stoicadavidioan@gmail.com>
2025-01-07 17:02:54 +0200

    Fixed an issue regarding parking

--- Assets/Scripts/Parking/ParkingHandler.cs
+++ Assets/Scripts/Parking/ParkingHandler.cs
@@ -39,7 +39,7 @@ public class ParkingZone : MonoBehaviour
 
                 //gameObject.SetActive(false);
                 StartCoroutine(PlayAndDeactivate());
-                if (ScoreManager.instance.pizzas == 1 && SceneManager.GetActiveScene().name != "Infnite Runner")
+                if (ScoreManager.instance.pizzas == 1 && SceneManager.GetActiveScene().name != "Infinite Runner")
                 {
                     Debug.Log(SceneManager.GetActiveScene().name);
                     GameOverController.isOver = true;
